0d16a228eab1e2e6d865c4fc362cfaa875544dfc
firibz <fariborzkorevli@gmail.com>
2023-05-03 01:54:31 +0330

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="dynamic-background">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+<!--    <img-->
+<!--      alt="Quasar logo"-->
+<!--      src="~assets/quasar-logo-vertical.svg"-->
+<!--      style="width: 200px; height: 200px"-->
+<!--    >-->
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
